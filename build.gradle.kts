63a555d534ab39230627aa234a4859682b99d1ea
dsult <yaroslav.vorobev-2015@mail.ru>
2025-03-25 21:19:11 +0300

    jdt test

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
 
     implementation("org.slf4j:slf4j-simple:1.7.36") // Добавляем привязку SLF4J
     implementation("guru.nidi:graphviz-java:0.18.1")
+
+    implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.40.0")
 }
 
 tasks.test {
